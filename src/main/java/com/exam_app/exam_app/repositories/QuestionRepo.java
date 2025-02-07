9d0fb3fd3e47bcabc0e1c6006d1d29750fddba6d
Trilok-Singh-Thakur-Practo <trilok.thakur@practo.com>
2025-02-07 23:44:24 +0530

    Question Handling Part Done

---
+++ src/main/java/com/exam_app/exam_app/repositories/QuestionRepo.java
@@ -0,0 +1,13 @@
+package com.exam_app.exam_app.repositories;
+
+import com.exam_app.exam_app.entities.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepo extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String Category);
+}
